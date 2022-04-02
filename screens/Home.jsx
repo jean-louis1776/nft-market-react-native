88cf05d5c2d2ab20674a83180cebd23375c0be32
ILYA <aleksin095@gmail.com>
2022-04-02 13:40:22 +0300

    added first files

---
+++ screens/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { View, SafeAreaView, FlatList, Text } from "react-native"
+import { COLORS, NFTData } from "../constants"
+
+function Home(props) {
+    return <Text>Home</Text>
+}
+export default Home
